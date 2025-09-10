80b7ba267bb39eb3129a38b43b8f67ef82cbe312
mi-pit <mi.pitner@gmail.com>
2025-09-10 11:48:34 +0200

    Add `src/headers/stdc_versions.h` for most common version macro values

---
+++ src/headers/stdc_versions.h
@@ -0,0 +1,25 @@
+/**
+ * @file attributes.h
+ * @brief
+ * Macros for values of the C standards.
+ *
+ * Written so I don't have to remember them.
+ */
+
+// Created by Michal Pitner on 10.09.2025.
+
+#ifndef STDC_VERSIONS_H
+#define STDC_VERSIONS_H
+
+
+#define STANDARD_C89_VERSION 199409L
+#define STANDARD_C99_VERSION 199901L
+#define STANDARD_C11_VERSION 201112L
+#define STANDARD_C17_VERSION 201710L
+#define STANDARD_C23_VERSION 202311L
+
+
+#define POSIX_C_LATEST_VERSION 200809L
+
+
+#endif //STDC_VERSIONS_H
